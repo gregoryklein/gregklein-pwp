d59252cda61537d605ed089a9cf167bb01731ae0
Greg Klein <gregkleinwrites@gmail.com>
2018-10-30 08:39:22 -0600

    added milestone2 document

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>PWP Milestone 2a for Greg Klein</title>
+	</head>
+	<body>
+		<h1>PWP Milestone 2a</h1>
+		<h2>Mobile Site Wireframe</h2>
+		<h2>Desktop Site Wireframe</h2>
+	</body>
+</html>
