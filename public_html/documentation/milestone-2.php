2c683a8e61b773a1c876034a1846045a2f88e3e8
Greg Klein <gregkleinwrites@gmail.com>
2018-10-31 13:55:49 -0600

    adding PWP milestone 2a content

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -5,8 +5,14 @@
 		<title>PWP Milestone 2a for Greg Klein</title>
 	</head>
 	<body>
-		<h1>PWP Milestone 2a</h1>
-		<h2>Mobile Site Wireframe</h2>
-		<h2>Desktop Site Wireframe</h2>
+		<h1>PWP Milestone 2a (Greg Klein)</h1>
+		<h2>Content Strategy</h2>
+		<p>My site will be a single page website on mobile and desktop.</p>
+		<p><strong>Landing Page: </strong>This page will show my name and the languages I use for full stack web development. An image below my name will show local landscape such as the Sandias. This landing page will link to my Portfolio page, my Contact Me page, and my Github page. </p>
+		<p><strong>Portfolio: </strong>This section will link to my team’s capstone project and will describe the languages used to create it and my role in the project.  The second image and accompanying text will show this site (my PWP) and describe the languages and tools used to create it. Clicking on the capstone image will take the user to the capstone site. The PWP image will not link to the PWP. This page will have a link back to the landing page and a link to the contact me page.</p>
+		<p><strong>Contact Me: </strong>This page will have icons linking to my Github page, my itch.io portfolio, and my YouTube channel. Below the social media links, a contact me form will allow the user to securely email me. This page will have links to my landing page and portfolio page.</p>
+		<h2>Wireframes</h2>
+		<p>This site will be responsive and will show the same content regardless of the screen size.</p>
+		<img src="https://www.dropbox.com/s/h3gatqnjkirgahi/PWPmockup.png?raw=1" alt="my PWP wireframes">
 	</body>
 </html>
