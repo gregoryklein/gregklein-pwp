dde2469fbf13e4de3c45d2e9af4efd69571a9a97
Greg Klein <gregkleinwrites@gmail.com>
2018-10-31 14:05:11 -0600

    changing mockup image

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -10,9 +10,9 @@
 		<p>My site will be a single page website on mobile and desktop.</p>
 		<p><strong>Landing Page: </strong>This page will show my name and the languages I use for full stack web development. An image below my name will show local landscape such as the Sandias. This landing page will link to my Portfolio page, my Contact Me page, and my Github page. </p>
 		<p><strong>Portfolio: </strong>This section will link to my team’s capstone project and will describe the languages used to create it and my role in the project.  The second image and accompanying text will show this site (my PWP) and describe the languages and tools used to create it. Clicking on the capstone image will take the user to the capstone site. The PWP image will not link to the PWP. This page will have a link back to the landing page and a link to the contact me page.</p>
-		<p><strong>Contact Me: </strong>This page will have icons linking to my Github page, my itch.io portfolio, and my YouTube channel. Below the social media links, a contact me form will allow the user to securely email me. This page will have links to my landing page and portfolio page.</p>
+		<p><strong>Contact Me: </strong>This page will have icons linking to my Github page, my itch.io portfolio, and my YouTube channel. Below the social media links, a contact me form will allow the user to securely email me. This page will have a link to my landing page and portfolio page.</p>
 		<h2>Wireframes</h2>
-		<p>This site will be responsive and will show the same content regardless of the screen size.</p>
-		<img src="https://www.dropbox.com/s/h3gatqnjkirgahi/PWPmockup.png?raw=1" alt="my PWP wireframes">
+		<p>This site will be responsive and will show the same content regardless of the screen size (except for the mobile contact page which does not have a portfolio link).</p>
+		<img src="https://www.dropbox.com/s/wmncuvvro23amfb/GregKleinPWPmockup.png?raw=1" alt="my PWP wireframes">
 	</body>
 </html>
